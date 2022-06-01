507c6022fb1e57185a020d4a488e6e962041fb09
DanielTruscaV1 <danieltrusca2016@gmail.com>
2022-06-01 20:21:19 +0300

    Published version 1 of the application

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
+//Import React
 import React from 'react';
+//Import React DOM (Document Object Model)
 import ReactDOM from 'react-dom/client';
+//Import CSS (Cascading Style Sheets)
 import './index.css';
+//Import the function component "App"
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+//Create a variable of type object that represents the HTML (Hyper Text Markup Language) root of the application
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//Call the "render" method of the "root" object
 root.render(
+  //Use strict mode
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
